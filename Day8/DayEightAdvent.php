3b64e2ab27c377da300c0b8667a1f441935dac1d
arvingasco <arvin.gasco@wedocode.co.uk>
2022-02-09 11:31:40 +0000

    day 8 part one cleaned up

--- Day8/DayEightAdvent.php
+++ Day8/DayEightAdvent.php
@@ -7,37 +7,23 @@ class DayEightAdvent
 {
     function partOne()
     {
-        $strings = [];
         $totalLiteralChar = 0;
         $totalStringChar = 0;
 
         foreach ($this->fileData() as $line) {
             $stringLiteral = trim($line);
             $totalLiteralChar += (strlen($stringLiteral));
-//            echo $stringLiteral;
-//            echo ' current string length ' . strlen($stringLiteral) . '<br>';
-//            echo 'literal total ' . $totalLiteralChar . ' <br>';
 
-            $string = trim($stringLiteral);
-            $string = ltrim($string, '"');
+            $string = ltrim($stringLiteral, '"');
             $string = substr($string, 0, -1);
-
             $string = preg_replace('/\\\x[a-f0-9]{2}/', '@', $string);
             $string = preg_replace('/\\\\\"/', '"', $string);
             $string = preg_replace('/\\\\\\\\/', '\\', $string);
 
             $totalStringChar += (strlen($string));
-//            echo '"' . $string . '"';
-//            echo ' current string length ' . strlen($string) . '<br>';
-//            echo 'string total ' . $totalStringChar . ' <br>';
-            $strings[] = $string;
         }
-//        foreach ($strings as $string) {
-//            echo $string . '<br>';
-//        }
         return $totalLiteralChar - $totalStringChar;
     }
-
 
     /**
      * @return Generator

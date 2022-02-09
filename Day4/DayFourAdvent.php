24b3f564fa805ccdd7883f6268c3c6de79a2e546
arvingasco <arvin.gasco@wedocode.co.uk>
2022-02-09 15:00:03 +0000

    clean up

--- Day4/DayFourAdvent.php
+++ Day4/DayFourAdvent.php
@@ -1,11 +1,25 @@
 <?php
 
 $dayFour = new DayFourAdvent();
-echo $dayFour->challenge();
+echo $dayFour->partOne();
+echo '<br>';
+echo $dayFour->partTwo();
 
 class DayFourAdvent
 {
-    function challenge(): string
+    function partOne(): string
+    {
+        $adventFile = $this->readFile();
+        $puzzleInput = fgets($adventFile);
+
+        $hexadecimal = 0;
+        $needle = '00000';
+        while (!str_starts_with(md5($puzzleInput . $hexadecimal), $needle))
+            $hexadecimal += 1;
+        return $hexadecimal;
+    }
+
+    function partTwo(): string
     {
         $adventFile = $this->readFile();
         $puzzleInput = fgets($adventFile);

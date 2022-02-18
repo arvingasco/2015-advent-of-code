2ede5db4b398ebec6914807d2a3601fb2d2b9d1a
arvingasco <arvin.gasco@wedocode.co.uk>
2022-02-18 16:12:19 +0000

    day 12 part 1

--- Day12/Day12Advent.php
+++ Day12/Day12Advent.php
@@ -1,6 +1,17 @@
 <?php
 
+$day12 = new Day12Advent();
+echo 'The sum of all the numbers is ' . $day12->partOne() . '.<br>';
+
 class Day12Advent
 {
+    function partOne(): int
+    {
+        $file = fopen('input.json', 'r');
+        $json = fgets($file);
 
+        preg_match_all('/[-0-9]+/', $json, $numbers);
+
+        return array_sum($numbers[0]);
+    }
 }

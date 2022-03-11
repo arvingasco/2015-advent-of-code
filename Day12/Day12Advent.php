45759fc6729d115bbef8dedd21c9626f1aa0a406
arvingasco <arvin.gasco@wedocode.co.uk>
2022-03-11 16:06:59 +0000

    day 12 part 2

--- Day12/Day12Advent.php
+++ Day12/Day12Advent.php
@@ -1,7 +1,16 @@
 <?php
 
 $day12 = new Day12Advent();
-echo 'The sum of all the numbers is ' . $day12->partOne() . '.<br>';
+
+echo 'The sum of all the numbers (part one) is ' . $day12->partOne() . '.<br>';
+
+$file = fopen('input.json', 'r');
+$json = fgets($file);
+fclose($file);
+$json = json_decode($json);
+
+echo 'The sum of all the numbers (part two) is ' . $day12->partTwo($json) . '.<br>';
+
 
 class Day12Advent
 {
@@ -9,9 +18,31 @@ class Day12Advent
     {
         $file = fopen('input.json', 'r');
         $json = fgets($file);
+        fclose($file);
 
         preg_match_all('/[-0-9]+/', $json, $numbers);
 
         return array_sum($numbers[0]);
+    }
+
+    function partTwo($json): int|string|null
+    {
+        $sum = 0;
+
+        foreach($json as $part) {
+            if(is_array($part) || is_object($part)) {
+                $sum += $this->partTwo($part);
+            }
+
+            if(is_numeric($part)) {
+                $sum += $part;
+            }
+
+            if(is_object($json) && is_string($part) && $part == "red") {
+                return null;
+            }
+        }
+
+        return $sum;
     }
 }
